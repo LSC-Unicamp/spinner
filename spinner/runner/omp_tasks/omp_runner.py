6fa799cff27ecfc43ad26ea541268e649807fd14
Rodrigo Ceccato de Freitas <rodrigoceccatodefreitas@gmail.com>
2024-07-17 19:40:28 -0300

    [DRAFT] Experimental template from config json

--- spinner/runner/omp_tasks/omp_runner.py
+++ spinner/runner/omp_tasks/omp_runner.py
@@ -1,5 +1,18 @@
 from runner.instance_runner import InstanceRunner
 import os
+from rich import print as rprint
+from jinja2 import Template, Environment, Undefined
+
+
+class StrictUndefined(Undefined):
+    def __getattr__(self, name):
+        raise NameError(f"'{name}' is undefined")
+
+    def __getitem__(self, name):
+        raise NameError(f"'{name}' is undefined")
+
+    def __str__(self):
+        raise NameError(f"'{self._undefined_name}' is undefined")
 
 
 class OmpRunner(InstanceRunner):
@@ -14,12 +27,28 @@ class OmpRunner(InstanceRunner):
         for n_tasks in self.config["tasks"]:
             for read_step in self.config["read_step"]:
                 input_file = self.metadata["input_file"]
+                rprint(f"self.metadata: {self.metadata}")
+                command_info = self.metadata["omp-tasks"]["command"]
+                template_string = command_info["template"]
+                rprint(f"command_template: {template_string}")
                 input_file = os.path.abspath(input_file)
                 bin_path = "./benchs/omp-tasks/build/lustre1"
                 bin_path = os.path.abspath(bin_path)
                 assert os.path.exists(
                     bin_path
                 ), f"Binary path {bin_path} does not exist"
+
+                env = Environment(undefined=StrictUndefined)
+                template = env.from_string(template_string)
+
+                command = template.render(
+                    bin_path=bin_path,
+                    input_file=input_file,
+                    tasks=n_tasks,  # rename for consistency
+                    read_step=read_step,
+                )
+                rprint(f"rendered command: {command}")
+                exit(1)
 
                 cmd = f"{bin_path} -f {input_file} -t {n_tasks} -r {read_step}"
                 instructions.append(
